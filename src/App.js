986351b6cf71b7286683d0626a6b2a55d0409123
santhoshsonu <santhosheshwaramma@gmail.com>
2022-05-15 12:45:49 +0530

    Use expenses list as state to render new expenses

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Expenses from './Components/Expenses/Expenses';
 import NewExpense from './Components/NewExpense/NewExpense';
 
 
+const basePrice = 120;
+const priceFactor = 10;
+const initalExpenses = [
+  {
+    id: 'e1',
+    title: 'New TV',
+    amount: (basePrice * Math.random() * priceFactor).toFixed(2),
+    date: new Date(2020, 7, 14)
+  },
+  {
+    id: 'e2',
+    title: 'Toilet Paper',
+    amount: (basePrice * Math.random() * priceFactor).toFixed(2),
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: (basePrice * Math.random() * priceFactor).toFixed(2),
+    date: new Date(2021, 2, 28)
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: (basePrice * Math.random() * priceFactor).toFixed(2),
+    date: new Date(2021, 5, 12)
+  }
+];
+
 const App = () => {
-  const basePrice = 120;
-  const priceFactor = 10;
 
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'New TV',
-      amount: (basePrice * Math.random() * priceFactor).toFixed(2),
-      date: new Date(2020, 7, 14)
-    },
-    {
-      id: 'e2',
-      title: 'Toilet Paper',
-      amount: (basePrice * Math.random() * priceFactor).toFixed(2),
-      date: new Date(2021, 2, 12)
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: (basePrice * Math.random() * priceFactor).toFixed(2),
-      date: new Date(2021, 2, 28)
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: (basePrice * Math.random() * priceFactor).toFixed(2),
-      date: new Date(2021, 5, 12)
-    }
-  ];
+  const [expenses, setExpenses] = useState(initalExpenses);
 
-  const addExpenseHandler = (newExpense) => {
-    newExpense['id'] = 'e' + (expenses.length + 1);
-    expenses.push(newExpense);
-    console.log(expenses);
+  const addExpenseHandler = (expense) => {
+    expense['id'] = 'e' + (expenses.length + 1);
+    setExpenses((prevState) => [expense, ...prevState]);
   };
 
   return (
