a59a4a9d3f32f2f1a89a281f40404b5fffd1f8a9
santhoshsonu <santhosheshwaramma@gmail.com>
2022-05-15 12:59:45 +0530

    Refactored to make code lean and clean

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -17,13 +17,18 @@ const Expenses = (props) => {
     const filteredExpenses = props.items.filter(expense =>
         expense.date.getFullYear().toString() === filteredYear);
 
+    let expensesContent = <p>No Expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense =>
+            <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount} />
+        );
+    }
+
     return (
         <Card className='expenses'>
             <ExpensesFilter selectedYear={filteredYear} onFilterChange={filterChangeHandler} />
-            {filteredExpenses.length === 0 && <p>No Expenses found.</p>}
-            {filteredExpenses.length > 0 && filteredExpenses.map(expense =>
-                <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount} />
-            )}
+            {expensesContent}
         </Card>
     );
 
