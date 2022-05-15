5d8c15b7770e2a2714e7030fbad44aeb969b9a17
santhoshsonu <santhosheshwaramma@gmail.com>
2022-05-15 18:52:25 +0530

    Enforce expense amount as number

--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -11,7 +11,7 @@ const NewExpense = (props) => {
     const saveExpenseDataHandler = (expense) => {
         const expenseData = {
             title: expense.title,
-            amount: expense.amount,
+            amount: +expense.amount,
             date: new Date(expense.date)
         };
         props.onAddExpense(expenseData);
